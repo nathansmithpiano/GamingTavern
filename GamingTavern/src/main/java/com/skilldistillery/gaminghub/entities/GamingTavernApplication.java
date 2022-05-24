ce7e5bb0160995e695723a13842b1fde7e0e9f2b
lightningclear <voh777@yahoo.com>
2022-05-24 11:00:52 -0600

    create and configure SPring Boot, add JPA project dependency

---
+++ GamingTavern/src/main/java/com/skilldistillery/gaminghub/entities/GamingTavernApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.gaminghub.entities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingTavernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingTavernApplication.class, args);
+	}
+
+}
---
+++ GamingTavern/src/test/java/com/skilldistillery/gaminghub/entities/GamingTavernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.gaminghub.entities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingTavernApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
