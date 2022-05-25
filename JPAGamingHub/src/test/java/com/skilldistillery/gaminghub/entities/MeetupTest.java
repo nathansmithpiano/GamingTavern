06f8ab64f4f6708b80dafc12f32806ac2d92f104
lightningclear <voh777@yahoo.com>
2022-05-25 17:17:03 -0600

    added meetup test

---
+++ JPAGamingHub/src/test/java/com/skilldistillery/gaminghub/entities/MeetupTest.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.gaminghub.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MeetupTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Meetup meetup;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAGamingHub");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		meetup = em.find(Meetup.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+	}
+
+	@Test
+	@DisplayName("Meetup mapping")
+	void test() {
+		assertNotNull(meetup);
+		assertNotNull(meetup.getName());
+		assertEquals("Free for all", meetup.getName());
+	}
+
+}
