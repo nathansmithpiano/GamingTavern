62a48d030ffdbb0e258d16655f75515702e43e81
Acorneld <acorneld@gmail.com>
2022-05-25 16:37:02 -0600

    added GnS to missing

--- JPAGamingHub/src/main/java/com/skilldistillery/gaminghub/entities/Platform.java
+++ JPAGamingHub/src/main/java/com/skilldistillery/gaminghub/entities/Platform.java
@@ -37,4 +37,96 @@ public class Platform {
 		super();
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDateTime getCreated() {
+		return created;
+	}
+
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+
+	public LocalDateTime getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(LocalDateTime updated) {
+		this.updated = updated;
+	}
+
+	public List<Alias> getAlias() {
+		return alias;
+	}
+
+	public void setAlias(List<Alias> alias) {
+		this.alias = alias;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(alias, created, description, enabled, id, name, type, updated);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Platform other = (Platform) obj;
+		return Objects.equals(alias, other.alias) && Objects.equals(created, other.created)
+				&& Objects.equals(description, other.description) && enabled == other.enabled && id == other.id
+				&& Objects.equals(name, other.name) && Objects.equals(type, other.type)
+				&& Objects.equals(updated, other.updated);
+	}
+
+	@Override
+	public String toString() {
+		return "Platform [id=" + id + ", enabled=" + enabled + ", name=" + name + ", type=" + type + ", description="
+				+ description + ", created=" + created + ", updated=" + updated + ", alias=" + alias + "]";
+	}
+
+	
+	
 }
