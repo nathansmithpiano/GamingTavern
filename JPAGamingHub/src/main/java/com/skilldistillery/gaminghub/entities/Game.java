b8776e898f2fd193a263b6ba996697c060d385ac
Acorneld <acorneld@gmail.com>
2022-05-25 16:54:46 -0600

    All GnS added to current stubbed entities

--- JPAGamingHub/src/main/java/com/skilldistillery/gaminghub/entities/Game.java
+++ JPAGamingHub/src/main/java/com/skilldistillery/gaminghub/entities/Game.java
@@ -2,6 +2,7 @@ package com.skilldistillery.gaminghub.entities;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -57,6 +58,86 @@ public class Game {
 		super();
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getRatingId() {
+		return ratingId;
+	}
+
+	public void setRatingId(int ratingId) {
+		this.ratingId = ratingId;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStudio() {
+		return studio;
+	}
+
+	public void setStudio(String studio) {
+		this.studio = studio;
+	}
+
+	public LocalDateTime getCreated() {
+		return created;
+	}
+
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+
+	public LocalDateTime getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(LocalDateTime updated) {
+		this.updated = updated;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public List<Meetup> getMeetups() {
+		return meetups;
+	}
+
+	public void setMeetups(List<Meetup> meetups) {
+		this.meetups = meetups;
+	}
+
 	public List<Alias> getAlias() {
 		return alias;
 	}
@@ -64,6 +145,36 @@ public class Game {
 	public void setAlias(List<Alias> alias) {
 		this.alias = alias;
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(alias, created, enabled, id, imageUrl, meetups, name, ratingId, studio, updated, url);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(alias, other.alias) && Objects.equals(created, other.created) && enabled == other.enabled
+				&& id == other.id && Objects.equals(imageUrl, other.imageUrl) && Objects.equals(meetups, other.meetups)
+				&& Objects.equals(name, other.name) && ratingId == other.ratingId
+				&& Objects.equals(studio, other.studio) && Objects.equals(updated, other.updated)
+				&& Objects.equals(url, other.url);
+	}
+
+	@Override
+	public String toString() {
+		return "Game [id=" + id + ", ratingId=" + ratingId + ", enabled=" + enabled + ", name=" + name + ", studio="
+				+ studio + ", created=" + created + ", updated=" + updated + ", imageUrl=" + imageUrl + ", url=" + url
+				+ ", meetups=" + meetups + ", alias=" + alias + "]";
+	}
+
+	
 	
 	
 
--- JPAGamingHub/src/main/java/com/skilldistillery/gaminghub/entities/Message.java
+++ JPAGamingHub/src/main/java/com/skilldistillery/gaminghub/entities/Message.java
@@ -33,4 +33,62 @@ public class Message {
 		super();
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public LocalDateTime getCreated() {
+		return created;
+	}
+
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+
+	public Chat getChat() {
+		return chat;
+	}
+
+	public void setChat(Chat chat) {
+		this.chat = chat;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(chat, contents, created, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Message other = (Message) obj;
+		return Objects.equals(chat, other.chat) && Objects.equals(contents, other.contents)
+				&& Objects.equals(created, other.created) && id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [id=" + id + ", contents=" + contents + ", created=" + created + ", chat=" + chat + "]";
+	}
+	
+	
+	
+
 }
