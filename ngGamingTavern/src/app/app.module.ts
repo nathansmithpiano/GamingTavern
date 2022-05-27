b42c2c8a4546d14c0e3728c7d75327def71accde
Acorneld <acorneld@gmail.com>
2022-05-27 14:35:17 -0600

    stubbing angular version

--- ngGamingTavern/src/app/app.module.ts
+++ ngGamingTavern/src/app/app.module.ts
@@ -1,10 +1,11 @@
+import { RouterModule } from '@angular/router';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule } from '@angular/forms';
 
@@ -13,10 +14,12 @@ import { FormsModule } from '@angular/forms';
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     MDBBootstrapModule.forRoot(),
     FormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
