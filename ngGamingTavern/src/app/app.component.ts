34f64e08302df06a30d2fb0c645b2509aeaa9e5a
lightningclear <voh777@yahoo.com>
2022-05-28 14:13:49 -0600

    merge conflicts resolved

--- ngGamingTavern/src/app/app.component.ts
+++ ngGamingTavern/src/app/app.component.ts
@@ -1,15 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-<<<<<<< HEAD
-  title = 'mdb-angular-ui-kit-free';
-=======
   // title = 'mdb-angular-ui-kit-free';
-  title = 'Gaming Tavern';
->>>>>>> de7fe29475ee55239b0d9796280d9dfe584e5122
+  title = "Gaming Tavern";
 }
--- ngGamingTavern/src/app/app.module.ts
+++ ngGamingTavern/src/app/app.module.ts
@@ -1,36 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 // MDB Modules
-import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
-import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
-import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
-import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
-import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
-import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
-import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
-import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
-import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+import { MdbAccordionModule } from "mdb-angular-ui-kit/accordion";
+import { MdbCarouselModule } from "mdb-angular-ui-kit/carousel";
+import { MdbCheckboxModule } from "mdb-angular-ui-kit/checkbox";
+import { MdbCollapseModule } from "mdb-angular-ui-kit/collapse";
+import { MdbDropdownModule } from "mdb-angular-ui-kit/dropdown";
+import { MdbFormsModule } from "mdb-angular-ui-kit/forms";
+import { MdbModalModule } from "mdb-angular-ui-kit/modal";
+import { MdbPopoverModule } from "mdb-angular-ui-kit/popover";
+import { MdbRadioModule } from "mdb-angular-ui-kit/radio";
+import { MdbRangeModule } from "mdb-angular-ui-kit/range";
+import { MdbRippleModule } from "mdb-angular-ui-kit/ripple";
+import { MdbScrollspyModule } from "mdb-angular-ui-kit/scrollspy";
+import { MdbTabsModule } from "mdb-angular-ui-kit/tabs";
+import { MdbTooltipModule } from "mdb-angular-ui-kit/tooltip";
+import { MdbValidationModule } from "mdb-angular-ui-kit/validation";
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
-=======
-import { NavComponent } from './components/page/nav/nav.component';
->>>>>>> de7fe29475ee55239b0d9796280d9dfe584e5122
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { NavComponent } from "./components/page/nav/nav.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent
-  ],
+  declarations: [AppComponent, NavComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -51,6 +46,6 @@ import { NavComponent } from './components/page/nav/nav.component';
     MdbValidationModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
