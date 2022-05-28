331efefe587b583413d66aa110752aa5c7c2e956
lightningclear <voh777@yahoo.com>
2022-05-27 18:11:23 -0600

    angular dependency addition

--- ngGamingTavern/karma.conf.js
+++ ngGamingTavern/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-gaming-tavern'),
+      dir: require('path').join(__dirname, './coverage/mdb-angular-ui-kit-free'),
       subdir: '.',
       reporters: [
         { type: 'html' },
