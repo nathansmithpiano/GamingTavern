c2812b76bb1c1fb8dc102e287fbfc8ca1304149c
lightningclear <voh777@yahoo.com>
2022-05-28 15:33:36 -0600

    save point for .gitignore, removed excess git ignore materials

--- ngGamingTavern/src/app/app.component.ts
+++ ngGamingTavern/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   // title = 'mdb-angular-ui-kit-free';
-  title = "Gaming Tavern";
+  title = 'Gaming Tavern';
 }
--- ngGamingTavern/src/environments/environment.prod.ts
+++ ngGamingTavern/src/environments/environment.prod.ts
@@ -1,9 +1,4 @@
 export const environment = {
-<<<<<<< HEAD
-  production: true
-};
-=======
   production: true,
   baseUrl: '/GamingTavern/'
 };
->>>>>>> de7fe29475ee55239b0d9796280d9dfe584e5122
