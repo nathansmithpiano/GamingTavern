040575a14334b6abca71d19449712aa325d52b21
lightningclear <voh777@yahoo.com>
2022-05-30 12:13:31 -0600

    minor changes to controllers and security config auth

--- GamingTavern/src/main/java/com/skilldistillery/gaminghub/controllers/UserController.java
+++ GamingTavern/src/main/java/com/skilldistillery/gaminghub/controllers/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.gaminghub.entities.User;
@@ -20,6 +21,7 @@ import com.skilldistillery.gaminghub.services.UserService;
 
 @RestController
 @CrossOrigin({ "*", "http://localhost" })
+@RequestMapping("api")
 public class UserController {
 
 	@Autowired
