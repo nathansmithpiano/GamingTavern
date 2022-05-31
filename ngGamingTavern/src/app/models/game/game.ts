40a700e24760a323805396ed31982486a6bcc08b
Acorneld <acorneld@gmail.com>
2022-05-31 12:54:59 -0600

    ALL WORKING, NEW SAVE POINT

--- ngGamingTavern/src/app/models/game/game.ts
+++ ngGamingTavern/src/app/models/game/game.ts
@@ -1,34 +1,5 @@
-<<<<<<< HEAD
-export class Game {
-    id: number;
-    enabled: boolean;
-    name: string;
-    studio: string;
-    image_url: string;
-    url: string;
-    created: string;
-    updated: string;
 
-    constructor(
-        id: number=0,
-    enabled: boolean=true,
-    name: string = "",
-    studio: string = "",
-    image_url: string = "",
-    url: string = "",
-    created: string = "",
-    updated: string = "",
-    ){
-        this.id = id;
-        this.enabled = enabled;
-        this.name = name;
-        this.studio = studio;
-        this.image_url = image_url;
-        this.url = url;
-        this.created = created;
-        this.updated = updated;
-    }
-=======
+
 import { Rating } from "../rating/rating";
 
 export class Game {
@@ -61,5 +32,5 @@ export class Game {
     this.created = created;
     this.updated = updated;
   }
->>>>>>> ac5768c592865b39d9a76f056b99d8afbc521c3a
+
 }
