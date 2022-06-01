aaf8b8d3b99ef2ac772d5b96ab7b2be93b2ea180
Acorneld <acorneld@gmail.com>
2022-06-01 16:12:25 -0600

    commit before pull for Nathan HTML template

--- ngGamingTavern/src/app/components/home/home.component.ts
+++ ngGamingTavern/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
+import { UserService } from 'src/app/services/user/user.service';
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { User } from "src/app/models/user/user";
 import { AuthService } from "src/app/services/auth/auth.service";
 
 @Component({
@@ -8,14 +10,62 @@ import { AuthService } from "src/app/services/auth/auth.service";
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
+
+  submitted = false;
+  isUsernameTaken: boolean = false;
+  user: User;
+  isUserLoaded: boolean;
+  newUser: User = new User();
+  
+
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private auth: AuthService
+    private auth: AuthService,
+    
   ) {}
 
   // add extra class styling here (i.e. mb-5, me-2, text-muted, bootstrap classes, etc)
   gridItemClass: string = "";
 
   ngOnInit(): void {}
+
+
+
+  register(user: User): void {
+    console.log('Registering user:');
+    console.log(user);
+    this.auth.register(user).subscribe({
+      next: (registeredUser) => {
+        this.auth.login(user.username, user.password).subscribe({
+          next: (loggedInUser) => {
+            this.router.navigateByUrl('/users/' + user.username);
+          },
+          error: (problem) => {
+            console.error('RegisterComponent.register(): Error logging in user:');
+            console.error(problem);
+          }
+        });
+      },
+      error: (fail) => {
+        console.error('RegisterComponent.register(): Error registering account');
+        console.error('user account already taken')
+        this.isUsernameTaken = true;
+        console.error(fail);
+      }
+    });
+
+  
+    function checkUsername(username: string) {
+      if(this.getUserByUsername(username)){
+        this.isUsernameTaken = true;
+      }
+      
+    }
+    
+function username(username: any, string: any) {
+  throw new Error('Function not implemented.');
+}
+}
+
 }
