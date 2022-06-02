5ba172f3ccc495ec9247022217b987e27a9e0f2b
Acorneld <acorneld@gmail.com>
2022-06-02 01:45:45 -0600

    lots of changes to home page, NEW SAVE POINT

--- ngGamingTavern/src/app/global-component.ts
+++ ngGamingTavern/src/app/global-component.ts
@@ -1,4 +1,5 @@
 export class GlobalComponent {
   public static gridItemClass: string = 'shadow-4-strong';
   public static rippleColor: string = 'hsl(0, 0%, 75%)';
+  public static customRounding: string = "rounded-5";
 }
