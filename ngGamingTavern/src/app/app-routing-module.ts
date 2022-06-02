51c667d89734d07e8abc70696e75800bc3dd2d66
NathanSmith <nathansmithpiano@gmail.com>
2022-06-02 14:06:06 -0600

    before reset for presentation

--- ngGamingTavern/src/app/app-routing-module.ts
+++ ngGamingTavern/src/app/app-routing-module.ts
@@ -14,7 +14,7 @@ import { MeetupComponent } from "./components/meetup/meetup.component";
 import { GameComponent } from "./components/game/game.component";
 
 const routes: Routes = [
-  { path: "", pathMatch: "full", component: HomeComponent },
+  { path: "", pathMatch: "full", component: SplashComponent },
   { path: "home", pathMatch: "full", component: HomeComponent },
   { path: "splash", pathMatch: "full", component: SplashComponent },
   { path: "login", pathMatch: "full", component: LoginComponent },
@@ -33,7 +33,7 @@ const routes: Routes = [
 
 @NgModule({
   // imports: [RouterModule.forRoot(routes, {useHash: true})],
-  imports: [RouterModule.forRoot(routes, { useHash: false })],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
